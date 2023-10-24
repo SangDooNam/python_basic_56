ab2d5578df5c8e6b19f8b388b52b74c06132aab4
SangDoo Nam <kexe009@hotmail.com>
2023-10-24 20:12:34 +0200

    finish

--- src/bubblesort.py
+++ src/bubblesort.py
@@ -1,4 +1,11 @@
 # TODO implement bubble sort
 
 def bubblesort(in_list: list) -> list:
-    pass
+    swaps = False
+    for i in range(len(in_list) -1 ):
+        if in_list[i] > in_list[i+1]:
+            in_list[i], in_list[i+1] = in_list[i+1], in_list[i]
+            swaps = True
+    if swaps:
+        bubblesort(in_list)
+    return in_list
